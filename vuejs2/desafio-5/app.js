a1e4166c074f8f47845372364bcb54747ad3b7c8
Fedner <91825533+jenedua@users.noreply.github.com>
2022-05-05 15:39:14 -0300

    condicionais 05-05-2022

---
+++ vuejs2/desafio-5/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        expressao: true, 
+        nomes: ['jean', 'pedro', 'sandy', 'jenasha'] ,
+        livro: {
+            titulo: 'O senhor dos aneis',
+            autor: 'J.R.R. Tolkiens',
+            volume: '3'
+        },
+        aluno: {
+            id: 10,
+            nome: 'Maria',
+            notas: [7.64, 8.63, 6.98, 9.21]
+        }
+    }
+})
