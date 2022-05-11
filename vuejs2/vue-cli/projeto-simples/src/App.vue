72be2ca63f1737d7ea70acf16e68329ce1424bb6
jenedua <jenasha29@gmail.com>
2022-05-11 09:14:03 -0300

    commit: 11-05-2022

---
+++ vuejs2/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '@'">Alterar</button>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data:  function() {
+    return {
+      titulo: 'Teste data usando Vue JS !!'
+
+    }
+  }
+  
+}
+</script>
+
+<style>
+ #app {
+   background-color: blueviolet;
+   color: aliceblue;
+ }
+</style>
